49b78995aecb29be31979c10c5c3c7e053dcac8f
klin-7 <klin.mail.7@gmail.com>
2018-10-09 18:35:57 +0300

    c1

--- klin_api.py
+++ klin_api.py
@@ -1,4 +1,16 @@
 from urllib.parse import parse_qs
+from wsgiref.simple_server import make_server
 
-parameters = parse_qs('q=free+code+repository&oq=free+code+repository', '')
-print(parameters)
+
+def web_api(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    output = b'fsdfsdf\n'
+    for item in environ:
+        print(item, ': ', environ[item], sep='')
+
+    return [output]
+
+
+if __name__ == '__main__':
+    http_srv = make_server('localhost', 55555, web_api)
+    http_srv.serve_forever()
