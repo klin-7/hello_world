f7f620c3896e86e704bd3d036f231269a0889b61
klin-7 <klin.mail.7@gmail.com>
2018-10-09 20:52:00 +0300

    c2

--- klin_api.py
+++ klin_api.py
@@ -1,14 +1,32 @@
-from urllib.parse import parse_qs
 from wsgiref.simple_server import make_server
+from html import escape
+import re
 
+def get_schedule(environ):
+    url_args = environ['web_api.url_args']
+    if url_args:
+        url_args_user = escape(url_args[0])
+    else:
+        url_args_user = ''
+    return [url_args_user.encode()]
+
+def not_found():
+    return [b'invalid request']
+
+url_dispatcher = [
+    (r'get/schedule/([a-zA-Z0-9 ]+)/?$', get_schedule)
+]
 
 def web_api(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    output = b'fsdfsdf\n'
-    for item in environ:
-        print(item, ': ', environ[item], sep='')
-
-    return [output]
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    for regex, callback in url_dispatcher:
+        match = re.search(regex, path)
+        if match is not None:
+            environ['web_api.url_args'] = match.groups()
+            return callback(environ)
+        else:
+            return not_found()
 
 
 if __name__ == '__main__':
